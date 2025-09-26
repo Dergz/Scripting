a8cc4726ebe2213acb7a805f89a0c0bbebff1fc4
Rosie <austyn176@outlook.com>
2025-09-25 22:45:20 -0400

    first commit

---
+++ Main_Menu.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+## Main Menu script that ties everything together also establishing links to dirs & Folders
+
+## STATIC LINKS
+ServerDir="/home/skarf/ModPacks"
+Archives="/home/skarf/Archives"
+Scripting="/home/skarf/Scripting"
+SubScripts="$Scripting/SubScripts"
+
+
+
+## Welcoming Message
+    echo "" 
+    echo "###############################"
+    echo "###                         ###"
+    echo "###      Server Picker      ###"
+    echo "###                         ###"
+    echo "###############################"
+    echo "" 
